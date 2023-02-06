11398d3be2968d000e78de2c57486c2e335db6aa
Sherab-Z <codebodhi34@gmail.com>
2023-02-06 14:21:38 +0100

    Complete exercise 03; pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringArr = string.split(''); // Turn string into an array of its characters
+  let reverseArr = stringArr.reverse(); // Reverse the array
+  let reverseString = reverseArr.join('') // Turn this reversed array back into a string
+  return reverseString;
 };
 
 // Do not edit below this line
