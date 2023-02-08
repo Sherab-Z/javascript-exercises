2c0f4a4945473bdd59ba875efb7568853f451f60
Sherab-Z <codebodhi34@gmail.com>
2023-02-08 23:22:02 +0100

    Fix bug, make tests work properly

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+  for (let el of array) {
+    for (let i = 0; i < arguments.length; i++) {
+      if (arguments[i] === el) {
+        array.splice(array.indexOf(el), 1);
+      }
+  }
+}
+return array;
 };
 
+/* Pseudocode:
+Use for...of to iterate over the array
+
+*/
 // Do not edit below this line
 module.exports = removeFromArray;
