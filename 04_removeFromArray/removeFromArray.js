b7462e29eeec9f89555417cd2aa75b929aacc23c
Sherab-Z <codebodhi34@gmail.com>
2023-02-10 13:22:56 +0100

    Complete exercise 04, pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,26 @@
-const removeFromArray = function(array) {
-  for (let el of array) {
-    for (let i = 0; i < arguments.length; i++) {
-      if (arguments[i] === el) {
-        array.splice(array.indexOf(el), 1);
+/* Pseudocode:
+Use for loops to iterate over the array and compare its els to arg array els. If there's a match with an array el, then remove that el from the array
+*/
+
+// Function accepts an array (required) and any number of additional args (optional), then removes all array els that exactly match any of the args
+const removeFromArray = function (array) {
+  const removalsArr = Array.from(arguments).slice(1); // Convert args obj to an array, excluding the initial array arg
+
+  arrayLoop: for (let i = 0; i < array.length; i++) {
+    // Iterate over all els in array
+    for (let j = 0; j < removalsArr.length; j++) {
+      // For the current el in array, iterate over removalsArr
+      if (array[i] === removalsArr[j]) {
+        // If the current el in array matches an arg in removalsArr, remove it from the array
+        array.splice(i, 1);
+        i--; // Because splice() removes an el, we need to decrement the index so i doesn't skip the next el
+        continue arrayLoop; // Get next array el
       }
+      continue; // If current array el doesn't match, do nothing and continue to next el in array
+    }
   }
-}
-return array;
+  return array; // Return potentially modified array
 };
 
-/* Pseudocode:
-Use for...of to iterate over the array
-
-*/
 // Do not edit below this line
 module.exports = removeFromArray;
