ac529ef197e6452f3f2036d931b65cf4f06a79f9
Sherab-Z <codebodhi34@gmail.com>
2023-02-11 23:44:29 +0100

    Clarify comments in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,8 @@
-const repeatString = function(string, num) { // This function has 2 args: string and num. Declare a variable repeatStr; A loop will repeat num times, each time concatenating the input string repeatStr. Return repeatStr.
+const repeatString = function(string, num) { // Takes 2 arguments: a string and a number (num), and returns the string repeated num times
   let repeatStr = ''; // Empty string to hold the output
 
   if (num >= 0) { // Check that num is not negative (it's impossible to concatenate a string negative times)
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < num; i++) { // Loop concatenates the string onto repeatStr num times
     repeatStr += string;
   }
   return repeatStr;
