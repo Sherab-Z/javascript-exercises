682026997b5dc54f29763e65e48c41a7289c33b6
Sherab-Z <codebodhi34@gmail.com>
2023-02-11 23:46:41 +0100

    Clarify comments in reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,8 @@
-const reverseString = function(string) {
+const reverseString = function(string) { // Returns reversed string
   let stringArr = string.split(''); // Turn string into an array of its characters
-  let reverseArr = stringArr.reverse(); // Reverse the array
-  let reverseString = reverseArr.join('') // Turn this reversed array back into a string
-  return reverseString;
+  let reversedArr = stringArr.reverse(); // Reverse the array
+  let reversedStr = reversedArr.join('') // Turn this reversed array back into a string
+  return reversedStr;
 };
 
 // Do not edit below this line
