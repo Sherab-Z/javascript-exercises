06472deba1097f27310d64bd73f6a6a435e5d01d
Sherab-Z <codebodhi34@gmail.com>
2023-03-25 21:35:57 +0100

    Complete 11_getTheTitles, pass test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
 
+//  FUNC: Takes an array of objects, 'books', and returns an array of titles
+const getTheTitles = function(booksArr) {
+  return booksArr.map(book => book.title);
 };
 
 // Do not edit below this line
